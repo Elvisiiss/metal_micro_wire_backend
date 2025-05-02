23767de50fb019f197bc4df0c23702aea41800a1
zyj <wenrenmuyuan@3106133216@qq.com>
2025-05-02 16:57:04 +0800

    初始化

---
+++ src/main/java/com/mmw/metal_micro_wire_backend/MetalMicroWireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mmw.metal_micro_wire_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalMicroWireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetalMicroWireBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmw/metal_micro_wire_backend/MetalMicroWireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmw.metal_micro_wire_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalMicroWireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
